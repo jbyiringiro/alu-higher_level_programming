08d8f64be99b190aee9bd3b20ebad1e729b9ce5b
jbyiringiro <j.byiringir@alustudent.com>
2024-08-06 13:14:27 -0700

    Python test driven

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -45,7 +45,7 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_string(self):
         """Test a string."""
-        string = "Josh"
+        string = 'Josh'
         self.assertEqual(max_integer(string), 'r')
 
     def test_list_of_strings(self):
