d755035e95e5b3782851f02cfe32b51e3da97f08
jbyiringiro <j.byiringir@alustudent.com>
2024-09-29 13:59:02 -0700

    js objects

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -4,4 +4,5 @@ const fs = require('fs');
 const args = process.argv.slice(2);
 const fileA = fs.readFileSync(args[0], 'utf8');
 const fileB = fs.readFileSync(args[1], 'utf8');
-fs.writeFileSync(args[2], fileA + fileB );
+const fileC = `${fileA}${fileB}`;
+fs.writeFileSync(args[2], fileC);
